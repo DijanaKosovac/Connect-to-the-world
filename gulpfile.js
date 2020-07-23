e019e6e4db890376511985128da625d958d6e011
DijanaKosovac <kosovacdijana@gmail.com>
2020-07-23 08:44:10 +0200

    Project initialized

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const { src, parallel, dest } = require('gulp');
+const browserSync = require('browser-sync').create();
+
+const jsPath = 'src/js/**/*.js';
+const cssPath = 'src/css/**/*.css';
+
+function copyHtml() {
+    return src('*.html').pipe(gulp.dest('dist'));
+}
+
+function imgTask() {
+    return src('src/images/*').pipe(imagemin()).pipe(gulp.dest('dist/src/images'));
+}
+
+function jsTask() {
+    return src(jsPath)
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist/src/js'));
+}
+
+function cssTask() {
+    return src(cssPath)
+        .pipe(sourcemaps.init())
+        .pipe(concat('style.css'))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('dist/src/css'));
+}
+
+function style() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style);
+    gulp.watch('**.html').on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.watch = watch;
+exports.build = parallel(copyHtml, imgTask, jsTask, cssTask);
